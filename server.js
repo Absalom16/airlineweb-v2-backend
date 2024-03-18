e57407130667110fcdcc98b01c63ebcda77e319c
Absalom16 <absalomlihasi@gmail.com>
2024-03-18 15:25:39 +0300

    .

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json(), function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const wss = new WebSocket.Server({ noServer: true });
 
 // MongoDB connection
